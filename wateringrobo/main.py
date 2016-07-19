11809a97ab717a61c5853158fe9850aefd22ca84
hua <mzhua78@hotmail.com>
2016-07-19 14:04:48 +0800

    test

--- wateringrobo/main.py
+++ wateringrobo/main.py
@@ -9,7 +9,9 @@ from robo.wateringRobo import robo
 def instruction(robo):
     robo.detected_soil_is_dry_then(Command.LIGHT_ON)
     robo.detected_soil_is_dry_then(Command.START_PUMP)
+    robo.detected_soil_is_dry_then(Command.BEEP)
     robo.detected_soil_is_wet_then(Command.LIGHT_OFF)
+    robo.detected_soil_is_wet_then(Command.MUTE)
     robo.detected_soil_is_wet_then(Command.STOP_PUMP)
     robo.start(27)
 
--- wateringrobo/robo/command.py
+++ wateringrobo/robo/command.py
@@ -1,7 +1,7 @@
 class Command:
     TURN_MOTOR_CLOCK_WISE = 'turn_motor_clock_wise'
     TURN_MOTOR_ANTI_CLOCK_WISE = 'turn_motor_anti_clock_wise'
-    LIGHT_ON = 'turn_motor'
+    LIGHT_ON = 'light_on'
     LIGHT_OFF = 'light_off'
     BEEP = 'beep'
     MUTE = 'mute'
